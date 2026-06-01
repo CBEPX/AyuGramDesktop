f72eef4fc9ce6d4045bc57d1dc617e233de1074c
Ivan <283494121+redactdeveloper@users.noreply.github.com>
2026-06-01 18:53:35 +0300

    Allow restoring hidden translate bar

--- Telegram/SourceFiles/window/window_peer_menu.cpp
+++ Telegram/SourceFiles/window/window_peer_menu.cpp
@@ -1055,14 +1055,8 @@ void Filler::addTranslate() {
 		|| !Core::App().settings().translateChatEnabled()) {
 		return;
 	}
-	const auto history = _peer->owner().historyLoaded(_peer);
-	if (!history
-		|| !history->translateOfferedFrom()
-		|| history->translatedTo()) {
-		return;
-	}
-	_addAction(tr::lng_context_translate(tr::now), [=] {
-		history->peer->saveTranslationDisabled(false);
+	_addAction(tr::lng_translate_settings_show(tr::now), [=] {
+		_peer->saveTranslationDisabled(false);
 	}, &st::menuIconTranslate);
 }
 
